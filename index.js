9d9074066174b989900fc79c8a20981e6e339f81
Fritz Austin <astos8@outlook.com>
2022-07-01 08:39:03 -0400

    add missing comments

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 
 // Select some elements...
+
+// changing page header to align left
 let header = document.querySelector('#page-header')
 header.style.textAlign ="left"
 
+// give img  border -radius 50px
 let dogImages = document.querySelectorAll('.dog-image')
 for (let i=0; i< dogImages.length; i++){
     dogImages[i].style.borderRadius ='50px'
 }
 
+ // changing the align of dog names
 let dogNames = document.querySelectorAll('.dog-name')
 for (let i=0; i< dogNames.length; i++){
     dogNames[i].style.textAlign = 'left'
 }
 
+// changing color of footer
 let footer = document.querySelector('.footer')
 footer.style.color ="purple"
+
+// flipping img
+for (let i=0; i< dogImages.length; i++){
+    dogImages[i].style.transform ='scaleY(-1)'
+}
+
